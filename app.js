4aed10480b4f51939204ca11f34e59baf29a554c
muminaslam <muminaslamlk@gmail.com>
2021-09-29 12:19:11 +0530

    undefined, null and referrence error

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-// String
-console.log( typeof "Hello World!");
-console.log( typeof "M");
-console.log( typeof'Hi! I am Aslam');
+// Referene Error
+console.log(name);
+// the console will throw an error that the varaiable "name" not defined, and it is called as reference error
 
-// Numbers
-console.log( typeof 1);
-console.log( typeof 1.25);
-console.log( typeof (-87.5);
+// Undefined
+var age
+console.log(age);
+// In here console will popup the variable age is 'undefined', becaue of no values are assigned to the variable
 
-// Boolean
-console.log( typeof true);
-console.log( typeof false);
+// Null
+var color = null;
+console.log(color);
+// Now console will throw the datatype of null to the output.
+
+/*
+    "Undefined" is the error thrown by the JavaScript engine to the user for clarification of that a variable is not defined.
+
+    "NUll" is the error where developers send to the users that a particular variable is a datatype of "NULL"
+ */
