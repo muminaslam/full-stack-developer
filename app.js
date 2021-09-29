fd3a9bfd2b92d055a7b03a8786cb73dc8d061482
muminaslam <muminaslamlk@gmail.com>
2021-09-29 12:34:48 +0530

    variable types - Var and Const

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-// Arrays
+// Variable Types
 
-var arr = [3, 5, 6,7];
+var age = 21;
+age = 22;
+console.log(age);
+// In var variable type a valua can be reassinged serveral times.
 
-console.log(arr);
-console.log(arr.length);
-console.log(arr.push(10));
-console.log(arr.pull());
+const idNo = 12545346;
+
+idNo = 34534534;
+console.log(IdNo);
+
+// IN const, a varaiable's value can be assigned only one time, can't the variable value after the declaration.
