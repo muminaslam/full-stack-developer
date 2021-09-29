75a18c403adb82bd46f3c2559f48fea58c24a8c7
muminaslam <muminaslamlk@gmail.com>
2021-09-29 12:28:22 +0530

    JavaScript - Arrays or non-primitive data types

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-// Referene Error
-console.log(name);
-// the console will throw an error that the varaiable "name" not defined, and it is called as reference error
+// Arrays
 
-// Undefined
-var age
-console.log(age);
-// In here console will popup the variable age is 'undefined', becaue of no values are assigned to the variable
+var arr = [3, 5, 6,7];
 
-// Null
-var color = null;
-console.log(color);
-// Now console will throw the datatype of null to the output.
-
-/*
-    "Undefined" is the error thrown by the JavaScript engine to the user for clarification of that a variable is not defined.
-
-    "NUll" is the error where developers send to the users that a particular variable is a datatype of "NULL"
- */
+console.log(arr);
+console.log(arr.length);
+console.log(arr.push(10));
+console.log(arr.pull());
