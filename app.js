98f69b6dfc18842c49e403de9b1db641a487e9eb
muminaslam <muminaslamlk@gmail.com>
2021-09-29 11:56:43 +0530

    primitive-datatypes

---
+++ app.js
@@ -0,0 +1,13 @@
+// String
+console.log( typeof "Hello World!");
+console.log( typeof "M");
+console.log( typeof'Hi! I am Aslam');
+
+// Numbers
+console.log( typeof 1);
+console.log( typeof 1.25);
+console.log( typeof (-87.5);
+
+// Boolean
+console.log( typeof true);
+console.log( typeof false);
