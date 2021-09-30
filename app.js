7a7e7badddcbd89a2ae5cb649f5182ec7868b322
muminaslam <muminaslamlk@gmail.com>
2021-09-30 15:01:26 +0530

    classList method

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var heading = document.getElementById("heading");
-var para = document.getElementsByClassName("para");
-var tagName = document.getElementsByTagName("h1");
-var pTag = document.getElementsByTagName("p");
-var query = document.querySelector(".para");
 
-console.log(typeof heading);
-// para.push("just");
-console.log(tagName);
-console.log(pTag);
-console.log(query);
+heading.style = "color:red;font-size:2em";
+
+console.log(heading.classList);
+heading.classList.add("subTopic");
+console.log(heading.classList);
+
