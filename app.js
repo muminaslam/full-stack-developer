71dd1a223fef9c936b0bdfb5618ff6f7accf1d0e
muminaslam <muminaslamlk@gmail.com>
2021-09-30 14:34:02 +0530

    DOM element selectors

--- app.js
+++ app.js
@@ -1,64 +1,11 @@
-/*
-    OPERATORS
-    =========
-    1. Arithmetic Operator
-    2. Assignment Operator
-    3. Comparison Operator
-    4. Logical Operator    
-*/
+var heading = document.getElementById("heading");
+var para = document.getElementsByClassName("para");
+var tagName = document.getElementsByTagName("h1");
+var pTag = document.getElementsByTagName("p");
+var query = document.querySelector(".para");
 
-/* Arithmetic Operator
-   =================== */
-
-   var a = 10;
-   var b = 20;
-
-   console.log("a + b = ", a + b);
-   console.log("a - b = ", a - b);
-   console.log("a * b = ", a * b);
-   console.log("a / b = ", a / b);
-   console.log("b % a = ", b % a);
-   console.log("a % b = ", a % b);
-
-    //Increment
-   console.log("a+=1: ", a+=1);
-   console.log("a++:", a++); //first console the value of a then increment
-   console.log("a: ", a);
-   console.log("++b: ", ++b);// First increment the value of b and then console
-   console.log("a--: ", a--);
-   console.log("a: ", a);
-
-   /*
-        Assignment Operator
-        ===================*/
-    console.log("a = 25:", a = 25);
-
-    /*
-        Comparison Operator
-        ===================*/
-
-        var x = 10;
-        var y = "10";
-
-    console.log("x == y: ", x == y);//Check the values of the two varaibles
-    console.log("a != b: ", a != b);
-    console.log("a > b: ", a > b);
-    console.log("a < b: ", a < b);
-    console.log("x === y: ", x === y);//Check the data type of the two variables
-
-    /*
-        Logical Operator
-        ================*/
-    console.log("AND Operator");
-    console.log("true && true: ", true && true);
-    console.log("true && false: ", true && false);
-    console.log("false && true: ", false && true);
-    console.log("false && false: ", false && false);
-
-    console.log("OR Operator");
-    console.log("true || true: " + true || true);
-    console.log("true || false: " + true || false);
-    console.log("false || true: " + false || true);
-    console.log("false || false: " + false || false);
-
-    
+console.log(typeof heading);
+// para.push("just");
+console.log(tagName);
+console.log(pTag);
+console.log(query);
