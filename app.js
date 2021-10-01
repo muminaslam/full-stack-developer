3a085de38d707911adc727389f36b5a3f6e85229
muminaslam <muminaslamlk@gmail.com>
2021-10-01 15:38:25 +0530

    Arrow function and simplification

--- app.js
+++ app.js
@@ -1,71 +1,29 @@
-function greet(){
-    console.log("Guten Morgen");
-}
+// Arrow function
 
-greet();
-
-// Function with input parameters
-
-function greet2(name){
-    console.log("Guten Nacht " + name);
-}
-
-greet2("Elon Mask");
-
-function details(name,age){
-    console.log(name, age);
-}
-
-details("Aslam");
-
-// Set default parameter values
-
-function personal(name = "Steve", age = "55"){
-    console.log("Hey! " + name + " your age is " + age);
-}
-
-personal("chingiskhan", 200);
-
-// Addition
-
-function sum(a, b){
-    console.log("a + b = ", a + b);
-}
-
-sum(20);// Number +  undefined returns a "NaN" value
-
-// Return statement
-
-function sum(a, b){
-    var add = a + b;
-    return add;
-}
-
-console.log(sum(20, 30));
-// OR
-
-let result = sum(30,40);
-console.log(result);
-
-// Function in as an expresstion
-
-
-console.log(add(50, 60));
-
-// function add(a = 0, b = 0){
+// let add = (a = 0, b = 0) => {
 //     let sum = a + b;
 //     return sum;
 // }
 
-let add = function(a = 0, b = 0){
-    let sum = a + b;
-    return sum;
+
+/*
+    Declare arrow function without curly brackets and return keyword, the arrow will return the output from the function
+*/
+let add = (a = 0, b = 0) => a + b;
+
+console.log(add(10, 40));
+
+// =================================
+
+const greet = (name) => {
+    return "Hi! " + name + " Guten Nacht";
 }
 
+console.log(greet("Elon"));
 
+// ==================================
 
-let greeting = function(name = "Peter"){
-    console.log("Hey! " + name + " Guten Morgen");
-}
+// If we have only one parameter, this mathod will work
+const greet2 = name => "Hi! " + name + " Guten Abend";
 
-console.log(greeting("Aslam"));
+console.log(greet2("Steve"));
