7c0d9e01dccadfa3ab4cd917610fb4b6fe1dfffe
muminaslam <muminaslamlk@gmail.com>
2021-10-01 15:12:23 +0530

    regular functions

--- app.js
+++ app.js
@@ -1,35 +1,71 @@
-
-// var list = document.querySelectorAll("ul")[0];
-/*
-    - quyerSelectorAll outputs an array like object
-    - To access the html element should mention the array position at the end
-    - querySelectorAll("ul")[0];
-    - In here the "ul" html element in the first array position of "0";
-*/
-
-
-// var list = document.getElementsByTagName("ul")[0];
-/*
-    - The problem you have is that getElementsByTagName() (note the plural implied by the 's' in the name of the function)
-    - returns an array of DOM nodes, not a single DOM node, which is what appendChild() expects to work on; 
-    - therefore you need to identify which of the array of nodes you want to work with:
-    - To access the html element should mention the array position at the end
-    - getElementsByTagName("ul")[0];
-    - In here the "ul" html element in the first array position of "0";
-*/
-
-
-var list = document.querySelector("ul");
-
-console.log(list);
-
-var toDoList = ["Go to GYM", "Practice the web development", "Do the assignments"];
-
-for(let i = 0; i < toDoList.length; i++){
-    console.log(toDoList[i]);
-    var liElement = document.createElement("li");
-    // console.log(liElement);
-    var content = document.createTextNode(toDoList[i]);
-    liElement.appendChild(content);
-    list.appendChild(liElement);
+function greet(){
+    console.log("Guten Morgen");
 }
+
+greet();
+
+// Function with input parameters
+
+function greet2(name){
+    console.log("Guten Nacht " + name);
+}
+
+greet2("Elon Mask");
+
+function details(name,age){
+    console.log(name, age);
+}
+
+details("Aslam");
+
+// Set default parameter values
+
+function personal(name = "Steve", age = "55"){
+    console.log("Hey! " + name + " your age is " + age);
+}
+
+personal("chingiskhan", 200);
+
+// Addition
+
+function sum(a, b){
+    console.log("a + b = ", a + b);
+}
+
+sum(20);// Number +  undefined returns a "NaN" value
+
+// Return statement
+
+function sum(a, b){
+    var add = a + b;
+    return add;
+}
+
+console.log(sum(20, 30));
+// OR
+
+let result = sum(30,40);
+console.log(result);
+
+// Function in as an expresstion
+
+
+console.log(add(50, 60));
+
+// function add(a = 0, b = 0){
+//     let sum = a + b;
+//     return sum;
+// }
+
+let add = function(a = 0, b = 0){
+    let sum = a + b;
+    return sum;
+}
+
+
+
+let greeting = function(name = "Peter"){
+    console.log("Hey! " + name + " Guten Morgen");
+}
+
+console.log(greeting("Aslam"));
