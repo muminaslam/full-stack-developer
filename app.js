e37a77a9d716cb5b806bfc5ba943255db9270282
muminaslam <muminaslamlk@gmail.com>
2021-10-05 13:31:09 +0530

    JavaScript Scope let and var

--- app.js
+++ app.js
@@ -1,21 +1,74 @@
-// Callback function
 
-let arr = ["Eat", "run", "sleep", "wake", "study"];
+/*
+    let variable type
+    Variable scope
+*/
+// ==========================
 
-// let callbackFunction = () => {
-//     console.log("Executed!");
+
+// let a = 20;
+// var b = 45;
+// console.log(a, b);
+// a = 30;
+// b = 15;
+// console.log(a, b);
+
+// if(true){
+//     var Name = "Elon Mask";
+//     // console.log(name);
 // }
 
-// arr.forEach(callbackFunction);
+// console.log(Name);
 
-// In here forEach() fucntion returns the array values/elements inside the array
-
-let callbackFunction = (list, index) => {
-    console.log(list, index);
+if(true){
+    let Name = "Steve Jobs";
 }
 
-// arr.forEach(callbackFunction);
+// console.log(Name);
 
-arr.forEach((list, index) => {
-    console.log(list, index);
-});
+// if(true){
+//     console.log(Name);
+// }
+
+// var - Global Scope
+// for(var i = 0; i <= 10; i++){
+
+// }
+// console.log(i);
+
+// let local scope
+
+// for(let i = 0; i < 10; i++){
+
+// }
+// console.log(i)
+
+// Can't access child scope from parent
+// But can access parent scope from child
+/*
+if(true){
+    // var car = "Lamborgini";
+    console.log(car); //prompt as "Undefined"
+
+    if(true){
+        var car = "Lamborgini";
+    }
+}
+*/
+
+const print = () => {
+    var car ="MG";
+
+    if(true){
+        console.log(car);
+    }
+
+    return car;
+}
+print();
+
+// console.log(car);
+
+
+
+
