dd1274ade2a500da1f562b5ea8e277012522cc03
muminaslam <muminaslamlk@gmail.com>
2021-10-05 15:10:05 +0530

    This keyword

--- app.js
+++ app.js
@@ -1,74 +1,81 @@
+// console.log(this);// "this" keyword is pointing to the window object
+// console.log(this.alert("Hello"));
+
+// JavaScript Object
 
 /*
-    let variable type
-    Variable scope
-*/
-// ==========================
-
-
-// let a = 20;
-// var b = 45;
-// console.log(a, b);
-// a = 30;
-// b = 15;
-// console.log(a, b);
-
-// if(true){
-//     var Name = "Elon Mask";
-//     // console.log(name);
-// }
-
-// console.log(Name);
-
-if(true){
-    let Name = "Steve Jobs";
-}
-
-// console.log(Name);
-
-// if(true){
-//     console.log(Name);
-// }
-
-// var - Global Scope
-// for(var i = 0; i <= 10; i++){
-
-// }
-// console.log(i);
-
-// let local scope
-
-// for(let i = 0; i < 10; i++){
-
-// }
-// console.log(i)
-
-// Can't access child scope from parent
-// But can access parent scope from child
-/*
-if(true){
-    // var car = "Lamborgini";
-    console.log(car); //prompt as "Undefined"
-
-    if(true){
-        var car = "Lamborgini";
+var car = {
+    model: "Lexus",
+    manufacture: "Toyota",
+    color: "white",
+    speed: function(a="100km/h"){
+        console.log(a);
     }
 }
-*/
-
-const print = () => {
-    var car ="MG";
-
-    if(true){
-        console.log(car);
-    }
-
-    return car;
-}
-print();
 
 // console.log(car);
+// console.log(typeof car);
+// console.log(car.model);
+// console.log(car.manufacture);
+// console.log(car.color);
+// car.speed("200Km/h");
 
+*/
 
+// ============================================================
+// Use "this" keyword inside the object
 
+/*
+var manufacture = "Tesla";
+var car = {
+    model: "Lexus",
+    manufacture: "Toyota",
+    color: "White",
+    speed: function(){
+        // console.log(this)
 
+        // console.log(this.manufacture);// Using "this" keyword now object is pointing to the property "manufactuere" inside the "car" object
+        let manufacture = "BMW";
+        console.log(manufacture);//It's referring to the variable outside the "car" object. So, output will be "Tesla" not "Toyota"
+    }
+}
+
+car.speed();
+// If we use "this" keyword in root or outside any function or object it will point to the globall "window" object
+
+// Now If we use "this" keyword inside the function. it will point to the object of "car"
+
+*/
+
+// ===============================================================
+// Template String
+/*
+var car = {
+    model: "GT-R",
+    manufacture: "Nissan",
+    color: "Silver",
+    speed: function(){
+        // console.log(this.model + " is manufactured by  " + this.manufacture);
+
+        // Template String
+        console.log(`${this.model} is manufacured by ${this.manufacture}`);
+    }
+}
+car.speed();
+*/
+
+// ========================================================
+
+var car = {
+    model: "GT-R",
+    manufacture: "Nissan",
+    color: "White",
+    speed: () => {
+        // "this" keyword inside the arrow function points to the window object instead of inside the "car" object
+        console.log(this);
+        console.log(`${this.model} was manufactured by ${this.manufacture}`);//The output will be "undefined wwas manufactured by undefined" because "this" kwyword is pointing to the global or window object instead of "car" object
+        console.log(`${car.model} was manufactured by ${car.manufacture}`);
+    }
+}
+
+car.speed();
